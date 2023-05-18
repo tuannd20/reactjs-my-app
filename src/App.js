efd7e9e917b3d88b24c5728a399af9adad66e74c
tuannd20 <duytuanndt20@gmail.com>
2023-05-18 16:19:27 +0700

    render list

--- src/App.js
+++ src/App.js
@@ -24,10 +24,42 @@ function App() {
   // };
 
   return (
-    <div>
+    <div className="blog-list">
+      {/* <Feature></Feature>
       <Feature></Feature>
-      <Feature></Feature>
-      <Feature></Feature>
+      <Feature></Feature> */}
+      <BlogItem
+        image="https://images.unsplash.com/photo-1543134471-52488faa85af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+        avatar="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+      ></BlogItem>
+      <BlogItem
+        image="https://images.unsplash.com/photo-1543134471-52488faa85af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+        avatar="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+      ></BlogItem>
+      <BlogItem
+        image="https://images.unsplash.com/photo-1543134471-52488faa85af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+        avatar="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+      ></BlogItem>
+      <BlogItem
+        image="https://images.unsplash.com/photo-1543134471-52488faa85af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+        avatar="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+      ></BlogItem>
+      <BlogItem
+        image="https://images.unsplash.com/photo-1543134471-52488faa85af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+        avatar="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+      ></BlogItem>
+      <BlogItem
+        image="https://images.unsplash.com/photo-1543134471-52488faa85af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+        avatar="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+      ></BlogItem>
+      <BlogItem
+        image="https://images.unsplash.com/photo-1543134471-52488faa85af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+        avatar="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+      ></BlogItem>
+      <BlogItem
+        image="https://images.unsplash.com/photo-1543134471-52488faa85af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+        avatar="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+      ></BlogItem>
     </div>
   );
   // return (
@@ -50,6 +82,33 @@ function App() {
   //     </header>
   //   </div>
   // );
+}
+
+function BlogItem(props) {
+  return (
+    <div className="">
+      <div className="blog-image">
+        <img src={props.image} alt="" className="blog-image-link" />
+      </div>
+      <div className="blog-footer">
+        <div className="blog-avatar-author">
+          <img src={props.avatar} alt="" className="blog-link-avatar" />
+        </div>
+        <div className="detail">
+          <div className="blog-title">
+            <h3 className="blog-title-element">
+              {props.title || "This is example of title"}
+            </h3>
+          </div>
+          <div className="blog-author">
+            <h4 className="blog-author-element">
+              {props.author || "This is example of author"}
+            </h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
