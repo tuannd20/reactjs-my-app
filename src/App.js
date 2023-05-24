187a55826db009c92754f380ab690ee8aeb948b4
tuannd20 <duytuanndt20@gmail.com>
2023-05-24 10:55:37 +0700

    styles in react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import BlogList from "./components/blog/BlogList";
+import Button from "./components/button/Button";
 import Counter from "./components/counter/Counter";
 import Product from "./components/form/Product";
 import Game from "./components/tictactoe/Game";
@@ -12,6 +13,8 @@ function App() {
       <Counter></Counter> */}
       <Game></Game>
       {/* <Product></Product> */}
+      <Button>Primary</Button>
+      <Button secondary>Secondary</Button>
     </div>
   );
 }
